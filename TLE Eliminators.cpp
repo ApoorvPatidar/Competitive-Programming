4c5e78ea02b8d8afbdd21712208e1751f44bfc1e
Apoorv Patidar <apoorvpatidar.ap24@gmail.com>
2024-03-04 19:12:07 +0530

    Blanck Spaces

--- TLE Eliminators.cpp
+++ TLE Eliminators.cpp
@@ -781,6 +781,38 @@ int main(){
     while(t--){
         int n;
         cin>>n;
-        
+        int arr[n];
+
+        int count = 0;
+        int maxi = 0;
+
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == 0){
+                count++;
+                maxi = max(maxi, count); 
+            }else{
+                count = 0;
+            }
+        }
+        cout<<maxi<<endl;        
+    }
+}
+
+
+// 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+
     }
 }
