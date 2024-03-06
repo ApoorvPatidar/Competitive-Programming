bca6f9e60d2cbbb721c745f0122975776a07e385
Apoorv Patidar <apoorvpaidar.ap24@gmail.com>
2024-03-07 02:20:05 +0530

    2 Questions

--- TLE Eliminators.cpp
+++ TLE Eliminators.cpp
@@ -824,3 +824,38 @@ int main()
             no;
     }
 }
+
+
+// Walking Master
+
+#include<bits/stdc++.h>
+using namespace std; 
+int main() {
+	cin.tie(0),cout.tie(0);
+	int t;
+	cin>>t;
+
+	while(t--) {
+		int a,b,c,d;
+		cin>>a>>b>>c>>d;
+		if(b<=d&&c<=a+d-b) {
+			cout<<(d-b)+(a+d-b-c)<<"\n";
+		} else {
+			cout<<"-1\n";
+		}
+	}
+}
+
+// We Need the Zero
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+
+    }
+}
