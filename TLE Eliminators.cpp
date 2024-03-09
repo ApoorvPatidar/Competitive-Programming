820acdbebb32c8675f7a21b5bdbfe093753bf5d7
Apoorv Patidar <apoorvpaidar.ap24@gmail.com>
2024-03-09 15:44:55 +0530

    1Q

--- TLE Eliminators.cpp
+++ TLE Eliminators.cpp
@@ -923,11 +923,45 @@ int main(){
 
 #include <bits/stdc++.h>
 using namespace std;
+ 
+string solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(__gcd(v[i],v[j])<=2) return "Yes";
+        }
+    }
+    return "No";
+}
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        cout<<solve();
+        cout << endl;
+    }
+    //cout<<__gcd(143,1261);
+    return 0;
+}
+
+// One and Two
+
+
+#include <bits/stdc++.h>
+using namespace std;
 int main(){
     int t;
     cin >> t;
 
     while(t--){
-        
+
     }
 }
