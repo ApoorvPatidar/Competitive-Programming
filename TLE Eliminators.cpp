b751e5898895f6bef308fda418f500d901e76199
Apoorv Patidar <apoorvpaidar.ap24@gmail.com>
2024-03-14 18:24:50 +0530

    31 Days of code Completed

--- TLE Eliminators.cpp
+++ TLE Eliminators.cpp
@@ -1100,11 +1100,19 @@ void solve() {
 
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    cin >> t;
-
-    while(t--){
-
-    }
+typedef long long ll;
+ 
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+ 
+	int t;
+  cin>>t;
+  while(t--){
+    int n,a,b;
+    cin>>n>>a>>b;
+    if(n-(a+b)>1 || (a==n && b==n)) cout<<"Yes"<<"\n";
+	else cout<<"No"<<"\n";
+  }
 }
+
+
